590ffa144055bed0f5908e6eb46d8862ad12b761
AceMaelStrom <ezhang330@gmail.com>
2026-06-19 23:44:32 -0700

    forgot to commit

--- card.js
+++ card.js
@@ -1,3 +1,20 @@
+let activecard = null;
+let offsetfromX = 0;
+let offsetfromY = 0;
+let layer = 0;
+
+const card = document.querySelector(".noodel");
+const stack = new Map();
+
+card.dataset.id = crypto.randomUUID();
+
+stack.set(card.dataset.id, {
+    parent: null,
+    children: []
+});
+
+
+
 function Animate(card) {
     let angleturn = 8;
     let frames = [8,-4,2,-1,0];
@@ -15,46 +32,162 @@ function Animate(card) {
     }, 35);
 };
 
-const card = document.querySelector(".noodel")
+function spawncard(){
+    
+    const button = document.getElementById("spawner");
+    const spawnarea = document.getElementById("workspace");
 
 
-let dragging = false;
-let offsetfromX;
-let offsetfromY;
+    //dummy cards
 
+    button.addEventListener("click", () => {
+        const newcard = document.createElement("div");
 
+        let ID = crypto.randomUUID();
+
+        newcard.className = "noodel";
+        newcard.dataset.id = ID;
+        newcard.style.position = "absolute";
+        newcard.innerHTML = `
+            <p>dummy </p>
+            <div class = "intranode">
+                <p>dummytext</p>
+            </div>
+            <p id="energylevel">Cost: </p>
+        `;
+        stack.set(ID,  {
+            parent: null, children: []
+        })
+
+        spawnarea.appendChild(newcard);
+
+        dragon(newcard);
+
+    });
+
+};
 
 /*moving script*/
 
-card.addEventListener("mousedown", (e) => {
-    let cardarea = card.getBoundingClientRect();
-    dragging = true;
-    offsetfromX = e.clientX - card.offsetLeft;
-    offsetfromY = e.clientY - card.offsetTop;
+function dragon(card) {
 
-    /*pivot establish*/
-
-    let pivotX = e.clientX - cardarea.left;
-    let pivotY = e.clientY - cardarea.top;
-
-    card.style.transformOrigin = `${pivotX}px ${pivotY}px`;
-
-    //animation!
-
-    Animate(card);
+    card.addEventListener("mousedown", (e) => {
+        activecard = card;
 
 
-});
+        let cardarea = card.getBoundingClientRect();
+
+        offsetfromX = e.clientX - card.offsetLeft;
+        offsetfromY = e.clientY - card.offsetTop;
+
+
+        console.log("cardID: ", activecard.dataset.id)
+
+        /*pivot establish*/
+
+        let pivotX = e.clientX - cardarea.left;
+        let pivotY = e.clientY - cardarea.top;
+
+        card.style.transformOrigin = `${pivotX}px ${pivotY}px`;
+
+        layer++;
+        card.style.zIndex = layer;
+        
+
+
+        //animation!
+
+        Animate(card);
+
+
+    });
+}
+
+
+//to detect overlay
+
+function overlapdetector(card1,card2) {
+    const c1 = card1.getBoundingClientRect();
+    const c2 = card2.getBoundingClientRect();
+    return !(
+        c1.right < c2.left ||
+        c1.left > c2.right ||
+        c1.bottom < c2.top ||
+        c1.top > c2.bottom
+    );
+
+
+};
+
+function stackcards(node1, node2){
+    const node1ID = node1.dataset.id
+    const node2ID = node2.dataset.id
+        
+    const node1info = stack.get(node1ID);
+    const node2info = stack.get(node2ID);
+
+    if (!node1info || !node2info) {
+        console.error("Card not found in stack map");
+        return;
+    }
+
+    node1info.parent = node2ID;
+    node2info.children.push(node1ID);
+
+
+    console.log(`${node2ID} is on ${node1ID}`)
+
+};
+
+function movestack(card,dx,dy) {
+    card.style.left = (card.offsetLeft + dx) + "px";
+    card.style.top = (card.offsetTop + dy) + "px";
+
+    const id = card.dataset.id;
+    const info = stack.get(card.dataset.id);
+
+    for (const childID of info.children) {
+
+        const childCard =
+            document.querySelector(`[data-id="${childID}"]`);
+
+        if (childCard) {
+            movestack(childCard, dx, dy);
+        };
+
+    };
+
+};
 
 document.addEventListener("mousemove", (e) => {
-    if (!dragging) return;
+        if (!activecard) return;
 
-    card.style.left = (e.clientX - offsetfromX) + "px";
-    card.style.top = (e.clientY - offsetfromY) + "px";
-});
+        activecard.style.left = (e.clientX - offsetfromX) + "px";
+        activecard.style.top = (e.clientY - offsetfromY) + "px";
+    });
 
 document.addEventListener("mouseup", () => {
-    dragging = false;
+        
+
+    const cards = document.querySelectorAll(".noodel");
+
+    if (!activecard) return;
+
+    for (const other of cards) {
+        if (other === activecard) continue;
+        if (overlapdetector(activecard, other)) {
+            stackcards(activecard, other);
+            movestack(activecard, )
+            break;
+        }
+    };
+
+    activecard = null;
 });
+
+
+dragon(card);
+
+spawncard();
 
 
